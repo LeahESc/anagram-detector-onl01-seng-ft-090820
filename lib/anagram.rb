cfd33dfa0c5c2fa6a14aa7768f74cf29d17fb6e8
Leah Schlackman <leahemily1@gmail.com>
2020-09-18 18:18:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,11 +10,15 @@ class Anagram
   # binding.pry
   
   def match(array)
-    @word = word
-    self.word = @word
-    word_arr = word.split("")
+    # @word = word
+    # self.word = @word
+    # word_arr = word.split("")
     array.each do |item| 
-      new_arr = item.split("")
+     if item.split("").char.sort == @word.split("").char.sort
+       new_arr = []
+       new_arr << item
+      else
+        new_arr
       # if new_arr == word_arr 
       #   true 
       # else
